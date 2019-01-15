07bf68b5f214a1699c5077df9817e3bc6cf69a27
Kacper Sagnowski <ksagnowski@gmail.com>
2019-01-15 16:26:01 +0000

    Add basic ViewController transition

---
+++ pathSequencer/MenuViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MenuViewController.swift
+//  pathSequencer
+//
+//  Created by Kacper Sagnowski on 1/15/19.
+//  Copyright © 2019 Kacper Sagnowski. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openGame(_ sender: Any) {
+        performSegue(withIdentifier: "openScene", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
